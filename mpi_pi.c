69a6120355d37074b0bf2f92f80c75f520325c35
gonzalo <gonzalo@localhost>
2011-05-14 15:05:42 -0300

    Trabajo finalizado. Calcula el valor de PI, y tira los tiempos y errores del c?lculo.
    --
    user: gonzalo
    branch 'default'
    changed mpi_pi.c
    changed mpi_pi.h

--- mpi_pi.c
+++ mpi_pi.c
@@ -1,8 +1,19 @@
+/*	MPI_PI calcula el valor de PI con error en base a un predefinido. 
+	Compilar: 
+		$ mpicc -Wall mpi_pi.c -o mpi_pi.x
+	Ejecutar:
+		$ mpirun -np X mpi_pi.x		#X = cantidad de procesos.
+
+	Alumno: Gonzalo Sánchez Marqués. Matrícula: 200405023.
+	Ing. en Computación. FCEFyN, UNC. 2011.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
 #include <sys/time.h>
+#include <sys/utsname.h>
 #include <mpi.h>
 #include "mpi_pi.h"
  
@@ -11,10 +22,6 @@ int main (int argc, char *argv[])
 	int proc_id, n_procs, limite_inf, limite_sup, n_dentro, i;
 	double pi_local, pi_total, x, y, z, error, start_time, end_time;
 
-	struct timeval stime;
-
-	limite_inf   = -1;
-	limite_sup   = 1;
 	n_dentro = 0;
 
 	/* Inicio MPI */
@@ -29,22 +36,25 @@ int main (int argc, char *argv[])
 	{
 		system("clear");
 		printf("## Implementación de Montecarlo para el cálculo de PI\n\n");
-		printf("## Total de procesadores:			%d\n", n_procs);
-		printf("## Total de tiradas al círculo:		%d\n\n", TIRADAS);
- 
-		start_time = MPI_Wtime();
+
+		obtener_info_sist();
+
+		printf("## Total de procesadores: %d\n", n_procs);
+		printf("## Comienzo del programa con %d tiradas.\n\n", TIRADAS);
 	}
 
 	/* Procedimiento: Obtener un numero aleatorio, calcular por Pitágoras si está dentro del
 	círculo, y si cumple, incrementar la cuenta... */
 
-	gettimeofday(&stime, NULL);
-	srand(stime.tv_usec * stime.tv_usec * stime.tv_usec * stime.tv_usec);
+	srand(time(NULL));
 
-	for (i = 1; i <= TIRADAS; i++)
+	if (proc_id == MASTER)
+		start_time = MPI_Wtime();
+
+	for (i = 1; i <= TIRADAS; ++i)
 	{
-		x = genera_aleatorio(limite_inf, limite_sup);
-		y = genera_aleatorio(limite_inf, limite_sup);
+		x = rand()/(double)RAND_MAX;
+		y = rand()/(double)RAND_MAX;
 
 		z = pow(x, 2) + pow(y, 2);
 
@@ -57,6 +67,10 @@ int main (int argc, char *argv[])
 
 	/* Actualizar el resultado, suma de todos los cálculos... */
 	MPI_Reduce(&pi_local, &pi_total, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
+	if (proc_id != MASTER)
+		printf("~ Se ha enviado el valor de PI: %g, del proceso: %d\n", pi_local, proc_id);
+
+	MPI_Barrier(MPI_COMM_WORLD);
 
 	/* Unicamente maestro: calcular la media, error, imprimir resultados. */
 	if (proc_id == MASTER)
@@ -78,7 +92,20 @@ int main (int argc, char *argv[])
 	return 0;
 }
 
-double genera_aleatorio(double a, double b) 
+/* Obtiene la info del sistema y a imprime en pantalla */
+void obtener_info_sist()
 {
-	return (((b - a) * ((double) rand()/(double) RAND_MAX)) + a);
+	int dont_care, mpi_subversion, mpi_version;
+	struct utsname info;
+
+	uname(&info);
+	dont_care = MPI_Get_version(&mpi_version,&mpi_subversion);
+
+	printf("## Arquitectura: \t%s\n",info.machine);
+	printf("## Sistema: \t%s\n", info.sysname);
+	printf("## Release : \t%s\n",info.release);
+	printf("## Version : \t%s\n",info.version);
+	printf("## MPI Version : \t%-d.%-d\n",mpi_version,mpi_subversion);
+	printf("\n");
+	return;
 }
--- mpi_pi.h
+++ mpi_pi.h
@@ -1,10 +1,15 @@
 #ifndef _MPI_PI_H_
 #define _MPI_PI_H_
 
+/* Valores locales */
 #define MASTER             0
 #define PI      3.1415926535
+
+/* TIRADAS es una constante definida, se puede cambiar su valor y recompilar */
 #define TIRADAS    100000000
 
+/* Funciones */
 double genera_aleatorio(double, double);
+void obtener_info_sist();
 
 #endif /* _MPI_PI_H_ */
